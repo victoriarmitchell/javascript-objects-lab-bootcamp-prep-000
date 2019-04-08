d45d34171cdf77e4e6877b18d845385393e93fc4
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 03:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign(object,{key: 'value'});
-  return newObj;
+  return Object.assign({},object,{[key]: value});
 }
