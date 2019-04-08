8c0b83a076479645e5bcc5dcc60e723185c006d4
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-08 03:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},object,{[key]: value});
 }
+
+function destructivelyupdateObjectWithKeyAndValue(object, key, value) {
+  object.key=value;
+  return object;
+}
